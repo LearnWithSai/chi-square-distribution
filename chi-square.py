dc1aceb7c0ee694eb17796d918cd75b7bf495089
LearnWithSai <saikiranmaddukuri18@gmail.com>
2026-02-02 11:25:41 +0530

    Update app title and description for Chi-Square

--- chi-square.py
+++ chi-square.py
@@ -4,14 +4,12 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.set_page_config(page_title='Confidence Interval Calculator', layout='wide')
+st.set_page_config(page_title='Chi-Square Visualization', layout='wide')
 
-st.title('Confidence Interval Calculator for Z Procedure')
+st.title('Chi-Square Distribution Visualization')
 
-st.write('''
-This app calculates the confidence interval for a population mean using a Z-procedure and displays a graph of the interval.
-''')
 
+degrees_freedom = 1
 with st.sidebar:
     st.header('Input Parameters')
     degrees_freedom = st.number_input('Degrees Of Freedom', value=1.0)
@@ -25,12 +23,8 @@ for i in range(0,degrees_freedom):
 # Display results
 st.write(f'Critical Value (z-score): {z_score:.2f}')
 
-# Plot the confidence interval
 fig, ax = plt.subplots(figsize=(3, 2))
 
 sns.kdeplot(x, clip=(x.min(),x.max()), ax=ax)
-
-ax.set_title('Chi-Square Distribution Visualization')
-ax.legend()
 
 st.pyplot(fig)
